7707774bcf3e1ea9d3331009354572cab32227fa
Baptiste Foezon <baptiste.foezon@gmail.com>
2023-01-09 16:31:41 +0100

    test

--- balance_ml.py
+++ balance_ml.py
@@ -2,8 +2,21 @@ import pandas as pd
 from keras.layers import Dense
 from keras.models import Sequential
 from matplotlib import pyplot as plt
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, plot_confusion_matrix
 from sklearn.model_selection import train_test_split
+
+
+class estimator:
+  _estimator_type = ''
+  classes_=[]
+  def __init__(self, model, classes):
+    self.model = model
+    self._estimator_type = 'classifier'
+    self.classes_ = classes
+  def predict(self, X):
+    y_prob= self.model.predict(X)
+    y_pred = y_prob.argmax(axis=1)
+    return y_pred
 
 # Load the data from the CSV file
 data = pd.read_csv('data.csv')
@@ -23,17 +36,16 @@ model.add(Dense(1, activation='sigmoid'))
 # Compile the model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+model.summary()
 # Train the model
 model.fit(X_train, y_train, epochs=10, batch_size=32)
 
 # Evaluate the model
 loss, accuracy = model.evaluate(X_test, y_test)
 print(f'Loss: {loss}, Accuracy: {accuracy}')
-cm = confusion_matrix(X_test, y_test)
-labels = ["balance", "no balance"]
-disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
-
-disp.plot(cmap=plt.cm.Blues)
+classifier = estimator(model, ["Balance", "No Balance"])
+figsize = (12,12)
+plot_confusion_matrix(estimator=classifier, X=X_test, y_true=y_test, cmap='Blues', normalize='true', ax=plt.subplots(figsize=figsize)[1])
 # Show the plot
 plt.show()
 
